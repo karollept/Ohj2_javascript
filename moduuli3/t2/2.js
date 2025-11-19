127ea284d31e4eb645143a9a0c7de23588671539
karollept <karoliina.lepisto@metropolia.fi>
2025-11-19 10:11:18 +0200

    mod2

--- moduuli3/t2/2.js
+++ moduuli3/t2/2.js
@@ -3,7 +3,7 @@ const list = ['First item', 'Second item', 'Third item'];
 
 list.forEach((text, index) => {
   const li_element = document.createElement('li');
-  li_element.textContent = text;
+  li_element.textContent = text;  // automaattinen mutta teknisesti voi kai poistaa
   if (index === 1) li_element.classList.add ('my-item');
   target.appendChild(li_element);
 });
